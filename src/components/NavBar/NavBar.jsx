cd213c3cc134ca6f2b0a9b59106e9574091a3299
solanamr <romerosolana@gmail.com>
2023-03-10 17:07:44 -0300

    home finished

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,20 +12,22 @@ const NavBar = () => {
 
   return (
     <nav className="py-5 ">
-      <div className="flex justify-between items-center pb-5">
-        <img src={logo} alt="" className="w-32 pt-5 ml-10" />
+        <div className="flex justify-between items-center">
+          <img src={logo} alt="" className="w-32 pt-5 ml-10" />
 
-        <div className="flex">
-            <img src={eng} alt="" className="w-8 h-8" />
-            <Switch
-              defaultChecked color="default" onClick={() => i18n.changeLanguage("en")}/>
-            <img src={es} alt="" className="w-8 h-8" />
+          <div className="flex">
+            <button onClick={() => i18n.changeLanguage("en")}>
+              <img src={eng} alt="" className="w-8 h-8 mr-5" />
+            </button>
+            <button onClick={() => i18n.changeLanguage("es")}>
+              <img src={es} alt="" className="w-8 h-8" />
+            </button>
             <button className="bg-yellow rounded-xl text-black font-bold px-3 py-1 text-sm mr-10 ml-32">
               Suscribite
             </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
   );
 };
 
