812bfe34b1a7b7d985f64e0f2c93a709a1ca6ce3
RocioGL33 <rocio33gl@gmail.com>
2023-03-24 11:06:14 -0300

    png

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
-import Scene1 from "../../icons/Scene1";
+import Scene2 from "../../icons/Scene2";
 import { useTranslation } from "react-i18next";
 import { fetchUser, postUser } from "../../redux/states/user/userSlice";
 
@@ -43,7 +43,7 @@ const Form = () => {
           {t("form.svg_subtitle")}
         </h3>
         <p></p>
-        {/*<Scene1 />*/}
+        <Scene2 />
       </div>
       <form
         onSubmit={handleSubmit(onSubmit)}
